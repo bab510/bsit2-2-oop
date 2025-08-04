a54bc55801f2a2f6927e3d9ea55956663a93382b
Bob Benedict B Agustero <bbagustero37666@liceo.edu.ph>
2025-08-04 11:55:36 +0800

    activity 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Book book1 = new Book("2005", "Bob Benedict", 100);
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 281);
+        Book book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book1.displayInfo();
+
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
