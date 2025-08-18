1b26edfe8ad7443c24201d88574e6510b1dcfdf3
Bob Benedict B Agustero <bbagustero37666@liceo.edu.ph>
2025-08-18 11:33:02 +0800

    manakosiractivity4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-    }
+public class Main{
+public static void main(String[] args) {
+    GradeCalculator calculator = new GradeCalculator();
+    double average = calculator.calculateAverage(85.5, 92.0, 78.5, 90.0);
+    String letterGrade = calculator.getLetterGrade(average);
+
+
+    calculator.displayResult("John Smith", average);
+    calculator.displayResult("John Smith", average, letterGrade);
+}
 }
